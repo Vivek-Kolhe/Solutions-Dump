5dd30f81a37f418668fb221967aabd2d7eb152d5
pookie <61515675+Vivek-Kolhe@users.noreply.github.com>
2021-04-06 15:52:48 +0530

    Basic Data Types.cpp

---
+++ HackerRank/CPP/Basic Data Types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long int b;
+    char c;
+    double d;
+    float f;
+    cin >> a >> b >> c >> f >> d;
+    // cout << a  << endl << b << endl << c << endl << f << endl << d << endl;
+    printf("%d\n%ld\n%c\n%lf\n%f\n", a, b, c, f, d);
+    return 0;
+}
