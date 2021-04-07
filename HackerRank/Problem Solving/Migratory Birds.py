2bea25ccfc138febe8f46be3615715abd80714c3
pookie <61515675+Vivek-Kolhe@users.noreply.github.com>
2021-04-07 15:41:57 +0530

    Migratory Birds.py

---
+++ HackerRank/Problem Solving/Migratory Birds.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the migratoryBirds function below.
+def migratoryBirds(arr):
+    return max(set(arr), key = arr.count)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
