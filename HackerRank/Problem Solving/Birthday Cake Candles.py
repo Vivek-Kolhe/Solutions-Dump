8dabd70aa6a37a5dc0f1f89a4f534f546bc69673
pookie <61515675+Vivek-Kolhe@users.noreply.github.com>
2021-04-07 15:33:58 +0530

    Birthday Cake Candles.py

---
+++ HackerRank/Problem Solving/Birthday Cake Candles.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    i = max(ar)
+    j = ar.count(i)
+    return j
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
