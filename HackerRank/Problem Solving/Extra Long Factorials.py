d956af3508b7b86c03ca0e5825fec4374503f0a4
pookie <61515675+Vivek-Kolhe@users.noreply.github.com>
2021-04-09 14:13:59 +0530

    Extra Long Factorials.py

---
+++ HackerRank/Problem Solving/Extra Long Factorials.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    i = math.factorial(n)
+    print(i)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    extraLongFactorials(n)
