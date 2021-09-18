57342c56c2c5b4871c6913333cb29915b15fefc6
pookie <61515675+Vivek-Kolhe@users.noreply.github.com>
2021-09-18 14:01:18 +0530

    Ruby Array - Addition.rb

---
+++ HackerRank/Ruby/Ruby Array - Addition.rb
@@ -0,0 +1,23 @@
+def end_arr_add(arr, element)
+    # Add `element` to the end of the Array variable `arr` and return `arr`
+    arr << element
+    return arr
+end
+
+def begin_arr_add(arr, element)
+    # Add `element` to the beginning of the Array variable `arr` and return `arr`
+    arr.unshift(element)
+    return arr
+end
+
+def index_arr_add(arr, index, element)
+    # Add `element` at position `index` to the Array variable `arr` and return `arr`
+    arr.insert(index, element)
+    return arr
+end
+
+def index_arr_multiple_add(arr, index)
+    # add any two elements to the arr at the index
+    arr.insert(index, 69, 69)
+    return arr
+end
