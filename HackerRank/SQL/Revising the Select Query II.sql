836b694701c81d2a7b01125765a13b4aa60ce4dd
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2022-05-20 16:46:19 +0530

    Revising the Select Query II.sql

---
+++ HackerRank/SQL/Revising the Select Query II.sql
@@ -0,0 +1,5 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
