f268e9541f94fed78b38d037ff7bc1ec2e12b8e8
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2022-05-20 17:41:10 +0530

    Employee Salaries.sql

---
+++ HackerRank/SQL/Employee Salaries.sql
@@ -0,0 +1,5 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT name FROM Employee WHERE salary > 2000 AND months < 10 ORDER BY employee_id ASC;
