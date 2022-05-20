16b7bc39e182b9fa72d62f2b5c8620ba1da293bf
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2022-05-20 17:45:08 +0530

    Weather Observation Station 12.sql

--- HackerRank/SQL/Weather Observation Station 12.sql
+++ HackerRank/SQL/Weather Observation Station 12.sql
@@ -2,4 +2,4 @@
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
-SELECT DISTINCT CITY FROM STATION WHERE NOT(CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%') AND NOT(CITY LIKE '%A' OR CITY LIKE '%E' OR CITY LIKE '%I' OR CITY LIKE '%O' OR CITY LIKE '%U')
+SELECT DISTINCT CITY FROM STATION WHERE NOT(CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%') AND NOT(CITY LIKE '%A' OR CITY LIKE '%E' OR CITY LIKE '%I' OR CITY LIKE '%O' OR CITY LIKE '%U');
