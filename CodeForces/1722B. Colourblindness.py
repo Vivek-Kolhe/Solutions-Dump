b480de24f50c51a1b62d7ef5686dc8f2ddb8c3cb
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2023-09-26 10:36:16 +0530

    1722B. Colourblindness.py

---
+++ CodeForces/1722B. Colourblindness.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+  n = int(input())
+  first = input()
+  second = input()
+  flag = True
+  for i in range(n):
+    if (first[i] == second[i] == "R") or ((first[i] == "B" or first[i] == "G") and second[i] == "B" or second[i] == "G"):
+      continue
+    else:
+      flag = False
+      print("NO")
+      break
+  if flag:
+    print("YES")
