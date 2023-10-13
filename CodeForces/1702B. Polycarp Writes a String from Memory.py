74502132ecfb60b05f51a2659348b1dc284ce321
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2023-10-13 15:37:08 +0530

    1702B. Polycarp Writes a String from Memory.py

--- CodeForces/1702B. Polycarp Writes a String from Memory.py
+++ CodeForces/1702B. Polycarp Writes a String from Memory.py
@@ -3,13 +3,20 @@ for _ in range(int(input())):
   n = len(s)
   curr = 0
   cnt = 0
-  while curr < n:
-    st = []
-    for i in range(curr, n):
-      if s[i] not in st and len(st) < 4:
-        st.append(s[i])
-      if s[i] not in st and len(st) == 3:
-        break
-    curr = i
-    cnt += 1
-  print(count)
+  if n == 1:
+    print(1)
+  else:
+    while curr < n - 1:
+      st = []
+      for i in range(curr, n):
+        if s[i] not in st and len(st) < 3:
+          st.append(s[i])
+          continue
+        if s[i] not in st and len(st) == 3:
+          break
+      curr = i
+      cnt += 1
+      if curr == n - 1:
+        if s[curr] not in st:
+          cnt += 1
+    print(cnt)
