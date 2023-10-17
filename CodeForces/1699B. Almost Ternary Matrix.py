0a3cd8c23d72ccd14111716d9fd9b1e232643d91
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2023-10-17 11:20:33 +0530

    1699B. Almost Ternary Matrix.py

--- CodeForces/1699B. Almost Ternary Matrix.py
+++ CodeForces/1699B. Almost Ternary Matrix.py
@@ -1,27 +1,6 @@
-def pmat(n, m, mat):
-  for i in range(n):
-    for j in range(m):
-      print(mat[i][j], end=" ")
-    print()
-
 for _ in range(int(input())):
   n, m = map(int, input().split())
-  combs = [[1, 0], [0, 1]]
-  if n == 2 and m == 2:
-    pmat(n, m, combs)
-  else:
-    mat = []
-    for i in range(2):
-      temp = []
-      for j in range(m // 2):
-        if j % 2 == 0:
-          temp += combs[i%2]
-        else:
-          temp += combs[i%2][::-1]
-      mat.append(temp)
-    for i in range(n - 2):
-      if i % 2 == 0:
-        mat.append(mat[-1])
-      else:
-        mat.append(mat[0])
-    pmat(n, m, mat)
+  for i in range(1, n + 1):
+    for j in range(1, m+  1):
+      print((i // 2 + j // 2) % 2, end=" ")
+    print()
