f98a5bf71b8d14d623f38b97d95f7a7bfe4ce6a7
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2023-10-20 10:54:02 +0530

    227B. Effective Approach.py

--- CodeForces/227B. Effective Approach.py
+++ CodeForces/227B. Effective Approach.py
@@ -3,11 +3,11 @@ arr = list(map(int, input().split()))
 m = int(input())
 queries = list(map(int, input().split()))
 v, p = 0, 0
-new = [0] * (10 ** 5 + 3)
+h = {}
 for i in range(n):
-  new[arr[i]] = i
+  h[arr[i]] = i
 for i in range(m):
-  temp = new[queries[i]]
+  temp = h[queries[i]]
   v += temp + 1
   p += n - temp
 print(v, p)
