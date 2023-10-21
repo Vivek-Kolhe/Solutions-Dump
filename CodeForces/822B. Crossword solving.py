8f2aca7ac6e81f78795a3cdcee8e10f8dbc58bd0
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2023-10-21 12:36:47 +0530

    Create 822B. Crossword solving.py

---
+++ CodeForces/822B. Crossword solving.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+s = input()
+t = input()
+h = {}
+for i in range(m - n + 1):
+  temp = []
+  cost = 0
+  ss = t[i:i+n]
+  for j in range(n):
+    if s[j] != ss[j]:
+      cost += 1
+      temp.append(j + 1)
+  h[cost] = temp
+key = min(h.keys())
+print(key)
+print(*h[key])
