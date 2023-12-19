37997a3f3d8adbf3bf3ef9e8b8ff9414e38f5822
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2023-12-19 12:09:27 +0530

    00. Template.go

---
+++ CodeForces/00. Template.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func Solve(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var t int
+	fmt.Fscan(in, &t)
+}
+
+func main() {
+	Solve(os.Stdin, os.Stdout)
+}
