488276f82d62c7bd22886a067c608e493546420f
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2023-12-20 17:03:10 +0530

    00. Template.go

--- CodeForces/00. Template.go
+++ CodeForces/00. Template.go
@@ -5,7 +5,42 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"math"
 )
+
+func Sieve(n int) []int {
+	isPrime := make([]bool, n+1)
+	for i := 2; i <= n; i++ {
+		isPrime[i] = true
+	}
+	isPrime[0], isPrime[1] = false, false
+	for p := 2; p <= int(math.Sqrt(float64(n))); p++ {
+		if isPrime[p] {
+			for i := p * p; i <= n; i += p {
+				isPrime[i] = false
+			}
+		}
+	}
+	res := []int{}
+	for i := 2; i <= n; i++ {
+		if isPrime[i] {
+			res = append(res, i)
+		}
+	}
+	return res
+}
+
+func IsPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i < int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
 
 func Solve(_r io.Reader, _w io.Writer) {
 	in := bufio.NewReader(_r)
