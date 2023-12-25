15834143c73956451a13f23aba72402864096b66
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2023-12-25 12:09:46 +0530

    1917B. Erase First or Second Letter.go

---
+++ CodeForces/1917B. Erase First or Second Letter.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Pair struct {
+	First  int
+	Second int
+}
+
+type Number interface {
+	int | int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func Min[T Number](nums ...T) T {
+	res := nums[0]
+	for _, v := range nums {
+		if res > v {
+			res = v
+		}
+	}
+	return res
+}
+
+func Max[T Number](nums ...T) T {
+	res := nums[0]
+	for _, v := range nums {
+		if res < v {
+			res = v
+		}
+	}
+	return res
+}
+
+func Abs[T Number](num T) T {
+	if num < 0 {
+		return num * -1
+	}
+	return num
+}
+
+func GCD(a, b int) int {
+	for a > 0 && b > 0 {
+		if a > b {
+			a %= b
+		} else {
+			b %= a
+		}
+	}
+	if a == 0 {
+		return b
+	}
+	return a
+}
+
+func Solve(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var t int
+	fmt.Fscan(in, &t)
+	for ; t > 0; t-- {
+		var n int
+		var s string
+		fmt.Fscan(in, &n)
+		fmt.Fscan(in, &s)
+		
+		seen := make(map[rune]int)
+		var ans int
+		for i, v := range s {
+			if _, ok := seen[v]; !ok {
+				seen[v]++
+				ans += (n - i)
+			}
+		}
+		fmt.Fprintln(out, ans)
+	}
+}
+
+func main() {
+	Solve(os.Stdin, os.Stdout)
+}
