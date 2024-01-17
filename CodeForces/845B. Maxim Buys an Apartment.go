e689c790997410038620c16637ee517ecc010b79
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2024-01-17 15:04:45 +0530

    845B. Maxim Buys an Apartment.go

---
+++ CodeForces/845B. Maxim Buys an Apartment.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Pair struct {
+	First  int
+	Second int
+}
+
+type Number interface {
+	int | int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func Min[T Number](nums ...T) T {
+	res := nums[0]
+	for _, v := range nums {
+		if res > v {
+			res = v
+		}
+	}
+	return res
+}
+
+func Max[T Number](nums ...T) T {
+	res := nums[0]
+	for _, v := range nums {
+		if res < v {
+			res = v
+		}
+	}
+	return res
+}
+
+func Abs[T Number](num T) T {
+	if num < 0 {
+		return num * -1
+	}
+	return num
+}
+
+func GCD(a, b int) int {
+	for a > 0 && b > 0 {
+		if a > b {
+			a %= b
+		} else {
+			b %= a
+		}
+	}
+	if a == 0 {
+		return b
+	}
+	return a
+}
+
+func Solve(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var n, k int
+	fmt.Fscan(in, &n, &k)
+
+	if k == 0 || k == n {
+		fmt.Fprintln(out, 0, 0)
+		return
+	}
+	fmt.Fprintln(out, 1, Min(n-k, 2*k))
+}
+
+func main() {
+	Solve(os.Stdin, os.Stdout)
+}
