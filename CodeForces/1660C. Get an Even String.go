d132b25121b7819d636c3002c7248fa77011f4c3
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2024-01-30 13:42:53 +0530

    1660C. Get an Even String.go

---
+++ CodeForces/1660C. Get an Even String.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Pair struct {
+	First  int
+	Second int
+}
+
+type Number interface {
+	int | int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func Min[T Number](nums ...T) T {
+	res := nums[0]
+	for _, v := range nums {
+		if res > v {
+			res = v
+		}
+	}
+	return res
+}
+
+func Max[T Number](nums ...T) T {
+	res := nums[0]
+	for _, v := range nums {
+		if res < v {
+			res = v
+		}
+	}
+	return res
+}
+
+func Abs[T Number](num T) T {
+	if num < 0 {
+		return num * -1
+	}
+	return num
+}
+
+func GCD(a, b int) int {
+	for a > 0 && b > 0 {
+		if a > b {
+			a %= b
+		} else {
+			b %= a
+		}
+	}
+	if a == 0 {
+		return b
+	}
+	return a
+}
+
+func Solve(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var t int
+	fmt.Fscan(in, &t)
+	for ; t > 0; t-- {
+		var s string
+		fmt.Fscan(in, &s)
+
+		n := len(s)
+		var m int
+		seen := make([]bool, 26)
+		for _, ch := range s {
+			if seen[ch-'a'] {
+				m += 2
+				for i := range seen {
+					seen[i] = false
+				}
+				continue
+			}
+			seen[ch-'a'] = true
+		}
+		fmt.Fprintln(out, n-m)
+	}
+}
+
+func main() {
+	Solve(os.Stdin, os.Stdout)
+}
