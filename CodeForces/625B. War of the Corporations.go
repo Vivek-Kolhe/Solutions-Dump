5f13b4ebdf6121aca7b9ab011a5d1fe01ad89685
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2024-02-06 12:35:31 +0530

    625B. War of the Corporations.go

---
+++ CodeForces/625B. War of the Corporations.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type Pair struct {
+	First  int
+	Second int
+}
+
+type Number interface {
+	int | int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func Min[T Number](nums ...T) T {
+	res := nums[0]
+	for _, v := range nums {
+		if res > v {
+			res = v
+		}
+	}
+	return res
+}
+
+func Max[T Number](nums ...T) T {
+	res := nums[0]
+	for _, v := range nums {
+		if res < v {
+			res = v
+		}
+	}
+	return res
+}
+
+func Abs[T Number](num T) T {
+	if num < 0 {
+		return num * -1
+	}
+	return num
+}
+
+func GCD(a, b int) int {
+	for a > 0 && b > 0 {
+		if a > b {
+			a %= b
+		} else {
+			b %= a
+		}
+	}
+	if a == 0 {
+		return b
+	}
+	return a
+}
+
+func Solve(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var s, ss string
+	fmt.Fscan(in, &s)
+	fmt.Fscan(in, &ss)
+	
+	fmt.Fprintln(out, strings.Count(s, ss))
+}
+
+func main() {
+	Solve(os.Stdin, os.Stdout)
+}
