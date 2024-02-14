2f002544b5943f8cc1f87d3bb5ee757ac83bf43b
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2024-02-14 12:08:04 +0530

    9131B. Make Equal.go

---
+++ CodeForces/9131B. Make Equal.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Pair struct {
+	First  int
+	Second int
+}
+
+type Number interface {
+	int | int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func Min[T Number](nums ...T) T {
+	res := nums[0]
+	for _, v := range nums {
+		if res > v {
+			res = v
+		}
+	}
+	return res
+}
+
+func Max[T Number](nums ...T) T {
+	res := nums[0]
+	for _, v := range nums {
+		if res < v {
+			res = v
+		}
+	}
+	return res
+}
+
+func Abs[T Number](num T) T {
+	if num < 0 {
+		return num * -1
+	}
+	return num
+}
+
+func GCD(a, b int) int {
+	for a > 0 && b > 0 {
+		if a > b {
+			a %= b
+		} else {
+			b %= a
+		}
+	}
+	if a == 0 {
+		return b
+	}
+	return a
+}
+
+func Solve(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var t int
+	fmt.Fscan(in, &t)
+	for ; t > 0; t-- {
+		var n, sum int
+		fmt.Fscan(in, &n)
+		arr := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(in, &arr[i])
+			sum += arr[i]
+		}
+
+		poss := sum / n
+		var buffer int
+		for _, v := range arr {
+			if v >= poss {
+				buffer += (v - poss)
+			} else {
+				if buffer >= poss-v {
+					buffer -= (poss - v)
+				}
+			}
+		}
+		if buffer == 0 {
+			fmt.Fprintln(out, "YES")
+		} else {
+			fmt.Fprintln(out, "NO")
+		}
+	}
+}
+
+func main() {
+	Solve(os.Stdin, os.Stdout)
+}
