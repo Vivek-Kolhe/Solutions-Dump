d5e0544a8b131ecc43129a564514ea0b60334c45
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2024-02-14 12:49:00 +0530

    1931C. Make Equal Again.go

---
+++ CodeForces/1931C. Make Equal Again.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Pair struct {
+	First  int
+	Second int
+}
+
+type Number interface {
+	int | int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func Min[T Number](nums ...T) T {
+	res := nums[0]
+	for _, v := range nums {
+		if res > v {
+			res = v
+		}
+	}
+	return res
+}
+
+func Max[T Number](nums ...T) T {
+	res := nums[0]
+	for _, v := range nums {
+		if res < v {
+			res = v
+		}
+	}
+	return res
+}
+
+func Abs[T Number](num T) T {
+	if num < 0 {
+		return num * -1
+	}
+	return num
+}
+
+func GCD(a, b int) int {
+	for a > 0 && b > 0 {
+		if a > b {
+			a %= b
+		} else {
+			b %= a
+		}
+	}
+	if a == 0 {
+		return b
+	}
+	return a
+}
+
+func Solve(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var t int
+	fmt.Fscan(in, &t)
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(in, &n)
+		arr := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(in, &arr[i])
+		}
+
+		var p, q int
+		q = n - 1
+		for i := 1; i < n; i++ {
+			if arr[i] == arr[i-1] {
+				p = i
+			} else {
+				break
+			}
+		}
+		for i := n-2; i >= 0; i-- {
+			if arr[i] == arr[i+1] {
+				q = i
+			} else {
+				break
+			}
+		}
+		if p == n-1 && q == 0 {
+			fmt.Fprintln(out, 0)
+			continue
+		}
+		if arr[p] == arr[q] {
+			p++
+			q--
+			fmt.Fprintln(out, Abs(q-p+1))
+			continue
+		}
+		if p+1 >= n-q {
+			fmt.Fprintln(out, n-p-1)
+		} else {
+			fmt.Fprintln(out, n-(n-q))	
+		}
+	}
+}
+
+func main() {
+	Solve(os.Stdin, os.Stdout)
+}
