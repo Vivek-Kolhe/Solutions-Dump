fd2eadcafdb7169cc61ed00e64a4db5e5d5eea83
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2024-04-25 14:00:39 +0530

    CSES - Range Xor Queries.cpp

---
+++ Others/CSES - Range Xor Queries.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+ 
+ll arr[200005];
+ll seg[4*200005];
+ 
+void build(int idx, int low, int high) {
+    if(low == high) {
+        seg[idx] = arr[low];
+        return;
+    }
+    int mid = (low + high) / 2;
+    build(2 * idx, low, mid);
+    build(2 * idx + 1, mid + 1, high);
+    seg[idx] = seg[2*idx] ^ seg[2*idx+1];
+}
+
+void update(int idx, int low, int high, int pos, ll val) {
+    if(pos < low || pos > high) {
+        return;
+    }
+    if(low == high) {
+        seg[idx] = val;
+        return;
+    }
+    int mid = (low + high) / 2;
+    update(2 * idx, low, mid, pos, val);
+    update(2 * idx + 1, mid + 1, high, pos, val);
+    seg[idx] = seg[2*idx] ^ seg[2*idx+1];
+}
+
+ll query(int idx, int low, int high, int l, int r) {
+    if(low > r || high < l) {
+        return 0;
+    }
+    if(l <= low && high <= r) {
+        return seg[idx];
+    }
+    int mid = (low + high) / 2;
+    return query(2 * idx, low, mid, l, r) ^ query(2 * idx + 1, mid + 1, high, l, r);
+}
+ 
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    build(1, 0, n - 1);
+    while(m > 0) {
+        int l, r;
+        cin >> l >> r;
+        l--;
+        r--;
+        cout << query(1, 0, n - 1, l, r) << endl;
+        m--;
+    }
+}
+ 
+int main() {
+    solve();
+    return 0;
+}
